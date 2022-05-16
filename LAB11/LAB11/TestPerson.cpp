5a6fe206cc2f621a4339631c6b591a5d6dd1252e
sockomode <81730735+sockomode@users.noreply.github.com>
2022-05-15 22:14:16 -0700

    Delete LAB11/LAB11 directory

--- LAB11/LAB11/TestPerson.cpp
+++
@@ -1,6 +0,0 @@
-#include "TestPerson.hpp"
-
-TestPerson::TestPerson(const string& newName, const int& newAge, const string& newGender, const int& newHeight) : Person(newName, newAge, newGender, newHeight)
-{
-
-}
